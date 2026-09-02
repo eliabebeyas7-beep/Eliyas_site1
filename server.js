2185216e4bd9a58cf5a015099b0bff0445d11af2
eliabebeyas7-beep <eliabebeyas7@gmail.com>
2026-09-02 16:25:59 +0300

    Change static file serving directory in server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 3000;
 const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
